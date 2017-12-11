e12b921c4b46a6880c553ecd503066c2fbda093c
Jabier Espinal <jabierespinal@gmail.com>
2017-12-11 16:21:52 +0000

    added more lessons

---
+++ IterateOverArraysMap/index.js
@@ -0,0 +1,8 @@
+var oldArray = [1,2,3,4,5];
+
+// Only change code below this line.
+
+var newArray = oldArray.map(function(val){   // reads into old array and applies to new array then assigns to val
+  return val + 3;  // adds 3 to every array instance 4,5,6,7,8
+  
+});
