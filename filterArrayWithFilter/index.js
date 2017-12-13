0ed918d6ed16e60ad72280396e30ff72d105df68
Jabier Espinal <jabierespinal@gmail.com>
2017-12-13 18:54:15 +0000

    added filterArrayWithFilter

---
+++ filterArrayWithFilter/index.js
@@ -0,0 +1,13 @@
+
+var oldArray = [1,2,3,4,5,6,7,8,9,10];
+
+// Only change code below this line.
+
+var newArray = oldArray.filter(function(val) {
+  return val < 6;
+});
+
+
+// <-------------Notes ---------------->
+// use filter in a new array to return values less then 5
+// do not change the old array
