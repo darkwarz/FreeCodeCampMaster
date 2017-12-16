931fe5b7c8ae14f59d4bf958b91ded9593525d88
Jabier Espinal <jabierespinal@gmail.com>
2017-12-16 02:06:08 +0000

    added concantenateArraysWithConcatentate

---
+++ concatenateArraywithConcat/index.js
@@ -0,0 +1,12 @@
+
+var oldArray = [1,2,3];
+var newArray = [];
+
+var concatMe = [4,5,6];
+
+// Only change code below this line.
+
+newArray = oldArray.concat(concatMe);
+
+// prints out for answer [1,2,3,4,5,6]
+//joins the two arrays together
