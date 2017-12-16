7661b0c4c19e1f236118e1d12bc34798c48965e9
Jabier Espinal <jabierespinal@gmail.com>
2017-12-16 02:10:58 +0000

    splitStrings into an array has been added

---
+++ splitStringsintoAnArray/index.js
@@ -0,0 +1,8 @@
+var string = "Split me into an array";
+var array = [];
+
+// Only change code below this line.
+
+array = string.split(' ');
+//split by using spaces
+// prints out result : "Split" "me" "into" "an" "array"
