cc513a610727cc9a552506aa532e71fd3b751058
Jabier Espinal <jabierespinal@gmail.com>
2017-12-16 02:16:04 +0000

    added joinStringSwithJoin

---
+++ joinStringswithJoin/index.js
@@ -0,0 +1,10 @@
+var joinMe = ["Split","me","into","an","array"];
+var joinedString = '';
+
+// Only change code below this line.
+
+joinedString = joinMe.join(" ");
+
+// take joinMe and join by spaces
+//prints result split me into an array 
+// takes all elements of the arrays and put it into one string
